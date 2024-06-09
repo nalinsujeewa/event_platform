43574f355ada8f57a8c17c5b32654f8e49199adb
Nalin Sujeewa <sujeewa_nalin@hotmail.com>
2024-06-09 01:10:23 +0100

    grid detail

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -23,8 +23,8 @@ const EventDetails = async ({
   });
   return (
     <>
-      <section className=" flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
-        <div className=" grid grid-cols-1 md:grid-cols-2 gap-6 px-4 2xl:max-w-7xl">
+      <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
+        <div className="grid grid-cols-1 md:grid-cols-2 px-4 2xl:max-w-7xl">
           <Image
             src={event.imageUrl}
             alt="hero-image"
