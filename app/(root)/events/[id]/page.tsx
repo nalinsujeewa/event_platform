1ab9d8b8cd584c8fed248d6b813f07029d6f587b
Nalin Sujeewa <sujeewa_nalin@hotmail.com>
2024-06-09 01:19:17 +0100

    detail page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,6 +1,5 @@
 import CheckoutButton from "@/components/shared/CheckoutButton";
 import Collection from "@/components/shared/Collection";
-import { Button } from "@/components/ui/button";
 import {
   getEventById,
   getRelatedEventsByCategory,
@@ -8,7 +7,6 @@ import {
 import { formatDateTime } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
-import React from "react";
 
 const EventDetails = async ({
   params: { id },
@@ -21,58 +19,64 @@ const EventDetails = async ({
     eventId: event._id,
     page: searchParams.page as string,
   });
+
   return (
     <>
       <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
-        <div className="grid grid-cols-1 md:grid-cols-2 px-4 2xl:max-w-7xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
           <Image
             src={event.imageUrl}
-            alt="hero-image"
+            alt="hero image"
             width={1000}
             height={1000}
             className="h-full min-h-[300px] object-cover object-center"
           />
-          <div className=" flex w-full flex-col gap-8 pt-5 md:pt-10">
-            <div className=" flex flex-col gap-6">
-              <h2 className=" h2-bold">{event.title}</h2>
-              <div className=" flex flex-col gap-3 sm:flex-row sm:items-center">
+
+          <div className="flex w-full flex-col gap-8 p-5 md:p-10">
+            <div className="flex flex-col gap-6">
+              <h2 className="h2-bold">{event.title}</h2>
+
+              <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
                 <div className="flex gap-3">
-                  <p className="p-bold rounded-full bg-green-500/10 px-5 py-2 text-green-700">
-                    {event.isFree ? "FREE" : `£${event.price}`}
+                  <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
+                    {event.isFree ? "FREE" : `$${event.price}`}
                   </p>
-                  <p className="p-medium-16 rounded-full bg-gray-500/10 px-4 py-2.5 text-gray-500">
+                  <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
                     {event.category.name}
                   </p>
                 </div>
+
                 <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
                   by{" "}
-                  <span className=" text-primary-500">
+                  <span className="text-primary-500">
                     {event.organizer.firstName} {event.organizer.lastName}
                   </span>
                 </p>
               </div>
             </div>
-            {/* Checkout button */}
+
             <CheckoutButton event={event} />
-            <div className=" flex flex-col gap-5">
-              <div className=" flex gap-2 md:gap-3">
+
+            <div className="flex flex-col gap-5">
+              <div className="flex gap-2 md:gap-3">
                 <Image
                   src="/assets/icons/calendar.svg"
                   alt="calendar"
                   width={32}
                   height={32}
                 />
-                <div className="p-medium-16 md:p-medium-16 lg:p-regular-16 flex-col flex-wrap items-center">
+                <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center">
                   <p>
                     {formatDateTime(event.startDateTime).dateOnly} -{" "}
                     {formatDateTime(event.startDateTime).timeOnly}
                   </p>
-                  <p className="">
+                  <p>
                     {formatDateTime(event.endDateTime).dateOnly} -{" "}
                     {formatDateTime(event.endDateTime).timeOnly}
                   </p>
                 </div>
               </div>
+
               <div className="p-regular-20 flex items-center gap-3">
                 <Image
                   src="/assets/icons/location.svg"
@@ -80,26 +84,29 @@ const EventDetails = async ({
                   width={32}
                   height={32}
                 />
-                <p className="p-medium-16 lg:p-regular-18">{event.location}</p>
+                <p className="p-medium-16 lg:p-regular-20">{event.location}</p>
               </div>
             </div>
-            <div className=" flex flex-col gap-2">
-              <p className=" text-lg text-gray-600">What You'll learn</p>
-              <p className="p-medium-16 lg:p-regular-16">{event.description}</p>
-              <p className="p-medium-16 lg:p-regular-16 truncate text-primary-500 underline mb-2">
+
+            <div className="flex flex-col gap-2">
+              <p className="p-bold-20 text-grey-600">What You'll Learn:</p>
+              <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
+              <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
                 {event.url}
               </p>
             </div>
           </div>
         </div>
       </section>
-      {/* EVENTS FROM SAME CATEGORY */}
-      <section className=" wrapper my-8 flex flex-col gap-8 md:gap-12">
+
+      {/* EVENTS with the same category */}
+      <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
         <h2 className="h2-bold">Related Events</h2>
+
         <Collection
           data={relatedEvents?.data}
           emptyTitle="No Events Found"
-          emptyStateSubtext="come back later"
+          emptyStateSubtext="Come back later"
           collectionType="All_Events"
           limit={3}
           page={searchParams.page as string}
